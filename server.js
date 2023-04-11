e6cfd70d8ad5612983f25c5a1e3dc4622dbcdb18
YogeshMun0304 <techyogimun@gmail.com>
2023-04-11 17:01:05 +0530

    check

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import ProductRoute from './routes/ProductRoutes.js'
 dotenv.config();
 connectDB();
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use("/api/v1/auth",authRoute)
